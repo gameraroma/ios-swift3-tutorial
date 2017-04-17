2fc99e22a642204781a9a256e0f351255f5151f0
Chakpiwat Pacharajindawat <chakpiwat.p@codium.co>
2017-04-17 10:53:02 +0700

    connect table cell to class

---
+++ PracticeTableView/ItemTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ItemTableViewCell.swift
+//  PracticeTableView
+//
+//  Created by Chakpiwat Pacharajindawat on 174//60 BE.
+//  Copyright © 2560 Codium. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLable: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
