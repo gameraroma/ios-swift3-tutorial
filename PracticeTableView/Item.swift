2dd9815bd7aa2563596e299e0962b87e6308695f
Chakpiwat Pacharajindawat <chakpiwat.p@codium.co>
2017-04-17 10:56:54 +0700

    create Item model

---
+++ PracticeTableView/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  PracticeTableView
+//
+//  Created by Chakpiwat Pacharajindawat on 174//60 BE.
+//  Copyright © 2560 Codium. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
