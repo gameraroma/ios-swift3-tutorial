472361010b9e26e691ee2235c4f359c00c30a75e
Chakpiwat Pacharajindawat <chakpiwat.p@codium.co>
2017-04-17 10:14:30 +0700

    add view controller

--- PracticeTableView/ViewController.swift
+++ PracticeTableView/ViewController.swift
@@ -10,16 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBAction func setTextLabel(_ sender: UIButton) {
+        nameLabel.text = nameTextField.text
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
